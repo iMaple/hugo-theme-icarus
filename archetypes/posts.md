ac67c8eb4f98227d7ed03f9315f8e568861c4248
iMaple <imaple.org@gmail.com>
2020-01-01 13:19:39 +0800

    First migration

---
+++ archetypes/posts.md
@@ -0,0 +1,20 @@
+---
+title: "{{ replace .Name "-" " " | title }}"
+author: ""
+
+description: ""
+tags: []
+keywords: []
+categories: []
+thumbnail: ""
+
+toc: true
+comment: true
+
+date: {{ .Date }}
+lastmod: {{ .Date }}
+
+draft: true
+
+---
+
---
+++ static/js/main.js
@@ -0,0 +1,43 @@
+(function ($) {
+
+    if (typeof (moment) === 'function') {
+        $('.article-meta time').each(function () {
+            $(this).text(moment($(this).attr('datetime')).fromNow());
+        });
+    }
+
+    $('.article > .content > table').each(function () {
+        if ($(this).width() > $(this).parent().width()) {
+            $(this).wrap('<div class="table-overflow"></div>');
+        }
+    });
+
+    function adjustNavbar() {
+        const navbarWidth = $('.navbar-main .navbar-start').outerWidth() + $('.navbar-main .navbar-end').outerWidth();
+        if ($(document).outerWidth() < navbarWidth) {
+            $('.navbar-main .navbar-menu').addClass('is-flex-start');
+        } else {
+            $('.navbar-main .navbar-menu').removeClass('is-flex-start');
+        }
+    }
+
+    adjustNavbar();
+    $(window).resize(adjustNavbar);
+
+    const $toc = $('#toc');
+    if ($toc.length > 0) {
+        const $mask = $('<div>');
+        $mask.attr('id', 'toc-mask');
+
+        $('body').append($mask);
+
+        function toggleToc() {
+            $toc.toggleClass('is-active');
+            $mask.toggleClass('is-active');
+        }
+
+        $toc.on('click', toggleToc);
+        $mask.on('click', toggleToc);
+        $('.navbar-main .catalogue').on('click', toggleToc);
+    }
+})(jQuery);
